99de13b863a61545279e7589a075127c5f4a7f4f
Ciro S. Costa <ciro9758@gmail.com>
2016-09-06 20:56:55 -0300

    changes base image to alpine

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,17 @@
 FROM nginx:1.10.1-alpine
 MAINTAINER Ciro S. Costa <ciro.costa@liferay.com>
 
-
 RUN set -x && \
   apk --update upgrade                                  &&  \
-  apk add git bash fcgiwrap spawn-fcgi                  &&  \
+  apk add git bash fcgiwrap spawn-fcgi git-gitweb       &&  \
+
+  adduser git -h /var/lib/git -D                        &&  \
+  adduser nginx git                                     &&  \
 
   git config --system http.receivepack true             &&  \
   git config --system http.uploadpack true              &&  \
-  git config --global user.email "gitserver@git.com"    &&  \
-  git config --global user.name "Git Server"
+  git config --system user.email "gitserver@git.com"    &&  \
+  git config --system user.name "Git Server"A
 
 
 ADD ./etc /etc
--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,11 @@ set -o xtrace
 
 readonly GIT_PROJECT_ROOT="/var/lib/git"
 readonly GIT_INITIAL_ROOT="/var/lib/initial"
-readonly FCGIPROGRAM=/usr/bin/fcgiwrap
+readonly GIT_HTTP_EXPORT_ALL=true
+readonly GIT_USER=git
+readonly GIT_GROUP=git
+
+readonly FCGIPROGRAM="/usr/bin/fcgiwrap"
 readonly USERID=nginx
 readonly SOCKUSERID=$USERID
 readonly FCGISOCKET=/var/run/fcgiwrap.socket
@@ -47,10 +51,11 @@ clean_git_root () {
 }
 
 initialize_services () {
-  /usr/bin/spawn-fcgi -s $FCGISOCKET -F 10 -u $USERID -U $USERID -g $USERID -- $FCGIPROGRAM 
-  chown -R nginx $GIT_PROJECT_ROOT
+  chown -R git:git $GIT_PROJECT_ROOT
+  chmod -R 775 $GIT_PROJECT_ROOT
 
-  exec nginx -g "daemon off;"
+  /usr/bin/spawn-fcgi -s $FCGISOCKET -F 4 -u $USERID -g $USERID -U $USERID -G $GIT_GROUP -- "$FCGIPROGRAM"
+  exec nginx
 }
 
 
